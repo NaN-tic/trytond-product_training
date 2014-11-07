ab207e3c9ff101f6ed7c80a804eaabc8d455c354
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:33:23 +0100

    Cleanup tests

--- product.py
+++ product.py
@@ -1,6 +1,6 @@
-#This file is part product_training module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part product_training module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval, Bool
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
-#This file is part product_training module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part product_training module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 from .test_product_training import suite
 
