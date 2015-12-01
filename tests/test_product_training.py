4f847e97cd9a275d01c3f2ebec6a568866962160
resteve <resteve@zikzakmedia.com>
2015-12-01 11:15:54 +0100

    Tests

--- tests/test_product_training.py
+++ tests/test_product_training.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part product_training module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the product_training module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ProductTrainingTestCase(unittest.TestCase):
+class ProductTrainingTestCase(ModuleTestCase):
     'Test Product Training module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('product_training')
-
-    def test0005views(self):
-        'Test views'
-        test_view('product_training')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'product_training'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         ProductTrainingTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
